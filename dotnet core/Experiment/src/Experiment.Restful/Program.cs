2c496e55f1713732e4914452d2781f74eb48dded
wang.na <wang.na@Ctrip.com>
2016-11-04 14:23:12 +0800

    add configuration support

--- dotnet core/Experiment/src/Experiment.Restful/Program.cs
+++ dotnet core/Experiment/src/Experiment.Restful/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Experiment.Restful
 {
@@ -12,8 +13,11 @@ namespace Experiment.Restful
     {
         public static void Main(string[] args)
         {
+			var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+			
             var host = new WebHostBuilder()
                 .UseKestrel()
+				.UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
